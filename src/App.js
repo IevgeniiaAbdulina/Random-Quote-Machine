58fbc395ce374d154b6f67541c29fa349fb7cd16
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-01-20 14:58:32 +0100

    Added Sass to Project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <header className="App-header">Hello!</header>
     </div>
   );
 }
