f3dab60f98efd615c0418200b3dd0af3bb124dad
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-01-20 21:46:36 +0100

    Added Structure of Application: JSX elements(quote-box, -text, -author and clickable elements(button, twitter link)). Added simple page style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello!</header>
-    </div>
+      <div id="quote-box" className="quote">
+        <h1 id="text" className="quote-text">"Quote text</h1>
+        <p id="author" className="quote-author">- Quote author</p>
+        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet quote</a><br />
+        <button id="new-quote">New quote button</button>
+      </div>
   );
 }
 
