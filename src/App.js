04d2d37c14cdfd327f0d8a3341eb44c074a57635
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-01-27 23:02:57 +0100

    Added JSX React Component Structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import quoteIc from "./img/quote-ic.svg";
+import twitterIc from "./img/twitter-ic.svg";
 
-function App() {
-  return (
-      <div id="quote-box" className="quote">
-        <h1 id="text" className="quote-text">"Quote text</h1>
-        <p id="author" className="quote-author">- Quote author</p>
-        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet quote</a><br />
-        <button id="new-quote">New quote button</button>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteItem: ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div id="quote-box" className="quote">
+          <div className="quote-text-box">
+            <img src={quoteIc} alt="quote icon" />
+            <h1 id="text" className="quote-text">{ }</h1>
+          </div>
+          <p id="author" className="quote-author">- { }</p>
+          <div className="buttons">
+            <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
+              <img src={twitterIc} alt="twitter icon" />
+            </a>
+            <button id="new-quote" className="button">New quote</button>
+          </div>
+        </div>
+        <div className="attribution">
+          <p>Challenge by <a href="https://github.com/freeCodeCamp/freeCodeCamp" target="_blank" rel="noreferrer">freeCodeCamp</a>.
+          Coded by<a href="https://github.com/IevgeniiaAbdulina"> Ievgeniia Abdulina</a>
+          </p>
+        </div>
       </div>
-  );
+    );
+  }
 }
 
 export default App;
